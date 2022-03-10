b0fae1cd6b53abd8c9d8f2b730c6e42dcf3b3636
Anastasiya <stasyaya@rambler.ru>
2022-03-10 17:08:29 +0300

    Homework 3 Seminar

---
+++ Ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int newnumber = 0;
+int num = number;
+
+if (number < 100000 && number > 9999)
+{
+    while (num > 0)
+    {
+        newnumber = newnumber * 10 + num % 10;
+        num = num / 10;
+    }
+    //Console.WriteLine(newnumber);
+    if (number == newnumber)
+        {
+        Console.WriteLine("Палиндром");
+        }
+    else
+        {
+        Console.WriteLine("Не палиндром");
+        }
+}
+else
+    Console.WriteLine("Ввели не пятизначное число");
+
+
---
+++ Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A(xa,ya,za)   B(xb,yb,zb)
+// Distance = Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za))
+Random random = new Random();
+
+var xa = random.Next(1000);
+int xb = random.Next(1000);
+int ya = random.Next(1000);
+int yb = random.Next(1000);
+int za = random.Next(1000);
+int zb = random.Next(1000);
+
+Console.WriteLine("A("+ xa + ";" + ya + ";" + za + ")");
+Console.WriteLine("B("+ xb + ";" + yb + ";" + zb + ")");
+
+double Distance = Math.Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za));
+Console.WriteLine("Расстояние между А и В - " + Distance);
